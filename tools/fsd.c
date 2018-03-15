ad3b6cf10db1e8cac76e08343a7b1c7df6b14ecf
Jasper Renow-Clarke <jasper@mrgsystems.co.uk>
2018-03-15 12:25:02 +0000

    Allow .FSD files to contain sectors from multiple heads despite lack of provision for storing physical and logical head information.

--- tools/fsd.c
+++ tools/fsd.c
@@ -17,7 +17,7 @@ Identifier: "FSD"  string literal 3 bytes
 
 For each track
 ==============
-   track_num: 1 byte
+   track_num: 1 byte ** PHYSICAL **
      num_sec: 1 byte (00 == unformated)
 
     readable: 1 byte (00 == unreadable, ff ==readable) **ONLY IF FORMATTED**
@@ -25,9 +25,9 @@ For each track
    If readable:
      For each sector
      ===============
-           Track_ID: 1 byte
+           Track_ID: 1 byte **LOGICAL**
         Head_number: 1 byte
-          Sector_ID: 1 byte
+          Sector_ID: 1 byte **LOGICAL*
       reported_size: 1 byte (2^{7+x}; 0 ==>128, 1 ==> 256, 2==>512, 3==>1024 etc)
           real_size: 1 byte (2^{7+x})
          error_code: 1 byte (0==No Error, &20==Deleted Data; &0E = Data CRC Error)
@@ -54,8 +54,8 @@ Decoding of the "creator" 5 byte field:
 void fsd_write(FILE *fsdfile, const unsigned char tracks)
 {
   unsigned char buffer[10];
-  unsigned char curtrack, cursector;
-  unsigned char numsectors;
+  unsigned char curtrack, curhead, cursector;
+  unsigned char numsectors, totalsectors;
   Disk_Sector *sec;
 
   struct tm tim;
@@ -84,58 +84,62 @@ void fsd_write(FILE *fsdfile, const unsigned char tracks)
   // Loop through tracks
   for (curtrack=0; curtrack<tracks; curtrack++)
   {
-    numsectors=diskstore_countsectors(curtrack, 0);
+    totalsectors=diskstore_countsectors(curtrack, 0)+diskstore_countsectors(curtrack, 1);
 
     // Track header
     buffer[0]=curtrack;
     fwrite(buffer, 1, 1, fsdfile);
 
-    if (numsectors>0)
+    if (totalsectors>0)
     {
-      buffer[0]=numsectors;
+      buffer[0]=totalsectors;
       fwrite(buffer, 1, 1, fsdfile);
 
       buffer[0]=FSD_READABLE;
       fwrite(buffer, 1, 1, fsdfile);
 
-      for (cursector=0; cursector<numsectors; cursector++)
+      for (curhead=0; curhead<2; curhead++)
       {
-        sec=diskstore_findnthsector(curtrack, 0, cursector);
-
-        if (sec!=NULL)
+        numsectors=diskstore_countsectors(curtrack, curhead);
+        for (cursector=0; cursector<numsectors; cursector++)
         {
-          // Sector header
-          buffer[0]=sec->logical_track;
-          buffer[1]=sec->logical_head;
-          buffer[2]=sec->logical_sector;
-          buffer[3]=sec->logical_size;
+          sec=diskstore_findnthsector(curtrack, curhead, cursector);
 
-          switch (sec->datasize)
+          if (sec!=NULL)
           {
-            case 128: buffer[4]=0; break;
-            case 256: buffer[4]=1; break;
-            case 512: buffer[4]=2; break;
-            case 1024: buffer[4]=3; break;
-            case 2048: buffer[4]=4; break;
-            case 4096: buffer[4]=5; break;
-            case 8192: buffer[4]=6; break;
-            case 16384: buffer[4]=7; break;
-            default: buffer[4]=sec->logical_size; break;
+            // Sector header
+            buffer[0]=sec->logical_track;
+            buffer[1]=sec->logical_head; // No provision for physical head
+            buffer[2]=sec->logical_sector;
+            buffer[3]=sec->logical_size;
+
+            switch (sec->datasize)
+            {
+              case 128: buffer[4]=0; break;
+              case 256: buffer[4]=1; break;
+              case 512: buffer[4]=2; break;
+              case 1024: buffer[4]=3; break;
+              case 2048: buffer[4]=4; break;
+              case 4096: buffer[4]=5; break;
+              case 8192: buffer[4]=6; break;
+              case 16384: buffer[4]=7; break;
+              default: buffer[4]=sec->logical_size; break;
+            }
+
+            buffer[5]=FSD_ERR_NONE;
+
+            // Flag if sector marked deleted
+            if (sec->datatype==0xf8)
+              buffer[5]|=FSD_ERR_DELETED;
+
+            fwrite(buffer, 1, 6, fsdfile);
+
+            fwrite(sec->data, 1, sec->datasize, fsdfile);
           }
-
-          buffer[5]=FSD_ERR_NONE;
-
-          // Flag if sector marked deleted
-          if (sec->datatype==0xf8)
-            buffer[5]|=FSD_ERR_DELETED;
-
-          fwrite(buffer, 1, 6, fsdfile);
-
-          fwrite(sec->data, 1, sec->datasize, fsdfile);
-        }
-        else
-        {
-          // TODO handle missing sector
+          else
+          {
+            // TODO handle missing sector
+          }
         }
       }
     }
