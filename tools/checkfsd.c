dc4fda58dc161f7bc717c13e0470f244cc97f09f
Jasper Renow-Clarke <jasper@mrgsystems.co.uk>
2018-03-20 11:28:29 +0000

    Removed global file handle and added dependancy on fsd.h

--- tools/checkfsd.c
+++ tools/checkfsd.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-FILE *fp;
+#include "fsd.h"
 
-unsigned char fsd_processheader()
+unsigned char fsd_processheader(FILE *fsdfile)
 {
   char magic[3];
   unsigned char creator[5];
   char titlechar;
   unsigned char numtracks;
 
-  fread(magic, 3, 1, fp);
+  fread(magic, 3, 1, fsdfile);
 
   if ((magic[0]!='F') || (magic[1]!='S') || (magic[2]!='D'))
   {
@@ -20,31 +20,31 @@ unsigned char fsd_processheader()
 
   printf("FSD magic detected\n");
 
-  fread(creator, 5, 1, fp);
+  fread(creator, 5, 1, fsdfile);
   printf("Created: %.2d/%.2d/%d by %d release %d unused %.2x\n", (creator[0]&0xf8)>>3, creator[2]&0x0f, ((creator[0]&0x07)<<8)|creator[1], (creator[2]&0xf0)>>4, (((creator[4]&0xc0)>>6)<<8)|creator[3], creator[4]&0x3f);
 
   printf("Title: \"");
-  fread(&titlechar, 1, 1, fp);
-  while ((titlechar!=0) && (!feof(fp)))
+  fread(&titlechar, 1, 1, fsdfile);
+  while ((titlechar!=0) && (!feof(fsdfile)))
   {
     printf("%c", titlechar);
-    fread(&titlechar, 1, 1, fp);
+    fread(&titlechar, 1, 1, fsdfile);
   }
 
   printf("\"\n");
 
-  fread(&numtracks, 1, 1, fp);
+  fread(&numtracks, 1, 1, fsdfile);
 
   printf("Tracks in FSD: %d\n", numtracks);
 
   return numtracks;
 }
 
-void fsd_processsector(const unsigned char readability)
+void fsd_processsector(FILE *fsdfile, const unsigned char readability)
 {
   unsigned char sectorheader[6];
 
-  fread(sectorheader, 4, 1, fp);
+  fread(sectorheader, 4, 1, fsdfile);
 
   printf("    C%d H%d S%d N%d", sectorheader[0], sectorheader[1], sectorheader[2], sectorheader[3]);
 
@@ -53,7 +53,7 @@ void fsd_processsector(const unsigned char readability)
     unsigned char databyte;
     int datasize, i;
 
-    fread(&sectorheader[4], 2, 1, fp);
+    fread(&sectorheader[4], 2, 1, fsdfile);
 
     printf(" (%d) code %.2x\n", sectorheader[4], sectorheader[5]);
 
@@ -62,7 +62,7 @@ void fsd_processsector(const unsigned char readability)
     printf("      [%d]", datasize);
     for (i=0; i<datasize; i++)
     {
-      fread(&databyte, 1, 1, fp);
+      fread(&databyte, 1, 1, fsdfile);
       printf("%c", ((databyte>=' ')&&(databyte<='~'))?databyte:'.');
     }
     printf("\n");
@@ -71,20 +71,20 @@ void fsd_processsector(const unsigned char readability)
     printf("\n");
 }
 
-void fsd_processtrack(const unsigned char track)
+void fsd_processtrack(FILE *fsdfile, const unsigned char track)
 {
   unsigned char trackheader[3];
   int sector;
 
   printf("\n  Track : ");
 
-  fread(trackheader, 2, 1, fp);
+  fread(trackheader, 2, 1, fsdfile);
 
   printf("%d (%d)", trackheader[0], track);
 
-  if (trackheader[1]!=0x00)
+  if (trackheader[1]!=FSD_UNFORMATTED)
   {
-    fread(&trackheader[2], 1, 1, fp);
+    fread(&trackheader[2], 1, 1, fsdfile);
 
     printf("(%.2x %sreadable)\n", trackheader[2], trackheader[2]==0?"un":"");
   }
@@ -98,13 +98,14 @@ void fsd_processtrack(const unsigned char track)
     printf("%d\n", trackheader[1]);
 
   for (sector=0; sector<trackheader[1]; sector++)
-    fsd_processsector(trackheader[2]);
+    fsd_processsector(fsdfile, trackheader[2]);
 }
 
 int main(int argc, char **argv)
 {
   unsigned char numtracks;
   unsigned char track;
+  FILE *fp;
 
   if (argc!=2)
   {
@@ -119,13 +120,13 @@ int main(int argc, char **argv)
     return 2;
   }
 
-  numtracks=fsd_processheader();
+  numtracks=fsd_processheader(fp);
 
   for (track=0; track<numtracks; track++)
   {
     if (feof(fp)) break;
 
-    fsd_processtrack(track);
+    fsd_processtrack(fp, track);
   }
 
   fclose(fp);
