4c307c143799352a43dcb3fec4fe653e677ebcf0
Jasper Renow-Clarke <jasper@mrgsystems.co.uk>
2018-04-06 16:22:27 +0100

    Added the data block type for debug

--- tools/mfm.c
+++ tools/mfm.c
@@ -251,7 +251,7 @@ void mfm_addbit(const unsigned char bit, const unsigned long datapos)
 
           if (mfm_debug)
           {
-            fprintf(stderr, "DATA block ");
+            fprintf(stderr, "DATA block %.2x ", mfm_blocktype);
             fprintf(stderr, "CRC %.2x%.2x ", mfm_bitstream[mfm_bitlen-2], mfm_bitstream[mfm_bitlen-2]);
 
             if (dataCRC==GOODDATA)
