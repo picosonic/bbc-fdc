5de8fdb2797f62d14490bb1172e73a924aa49d3e
Jasper Renow-Clarke <jasper@mrgsystems.co.uk>
2019-02-05 10:42:49 +0000

    Added configurable CRC16 stream algorithm

--- tools/crc.c
+++ tools/crc.c
@@ -1,17 +1,23 @@
 #include "crc.h"
 
-// CCITT CRC16 (Floppy Disk Data)
-unsigned int calc_crc(unsigned char *data, int datalen)
+// Configurable CRC16 stream algorithm
+unsigned int calc_crc_stream(unsigned char *data, int datalen, unsigned int initial, unsigned int polynomial)
 {
-  unsigned int crc=0xffff;
+  unsigned int crc=initial;
   int i, j;
 
   for (i=0; i<datalen; i++)
   {
     crc ^= data[i] << 8;
     for (j=0; j<8; j++)
-      crc = (crc & 0x8000) ? (crc << 1) ^ 0x1021 : crc << 1;
+      crc = (crc & 0x8000) ? (crc << 1) ^ polynomial : crc << 1;
   }
 
   return (crc & 0xffff);
+}
+
+// CCITT CRC16 (Floppy Disk Data)
+unsigned int calc_crc(unsigned char *data, int datalen)
+{
+  return (calc_crc_stream(data, datalen, 0xffff, 0x1021));
 }
--- tools/crc.h
+++ tools/crc.h
@@ -1,6 +1,7 @@
 #ifndef _CRC_H_
 #define _CRC_H_
 
+extern unsigned int calc_crc_stream(unsigned char *data, int datalen, unsigned int initial, unsigned int polynomial);
 extern unsigned int calc_crc(unsigned char *data, int datalen);
 
 #endif
