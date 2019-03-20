9f9a8b970a9eb2a26343b2a8a4c1e2a766bf7fc2
Jasper Renow-Clarke <jasper@mrgsystems.co.uk>
2019-03-20 15:33:53 +0000

    Add hardware RPM to histogram debug

--- tools/mod.c
+++ tools/mod.c
@@ -32,7 +32,7 @@ void mod_buildhistogram(const unsigned char *sampledata, const unsigned long sam
   unsigned long datapos;
 
   if (mod_debug)
-    fprintf(stderr, "Creating histogram for track %d data sampled at %lu\n", hw_currenttrack, hw_samplerate);
+    fprintf(stderr, "Creating histogram for track %d data sampled at %lu with %.2f rpm\n", hw_currenttrack, hw_samplerate, hw_rpm);
 
   // Clear histogram
   for (j=0; j<MOD_HISTOGRAMSIZE; j++) mod_hist[j]=0;
