4db5dec8161fdb267ad7fa79ee4899afcee03251
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2020-10-21 11:27:37 +0100

    Added CRC32

--- tools/crc.c
+++ tools/crc.c
@@ -1,7 +1,8 @@
+#include <stdint.h>
 #include "crc.h"
 
 // Configurable CRC16 stream algorithm
-unsigned int calc_crc_stream(const unsigned char *data, const int datalen, const unsigned int initial, const unsigned int polynomial)
+uint16_t calc_crc_stream(const unsigned char *data, const int datalen, const uint16_t initial, const uint16_t polynomial)
 {
   unsigned int crc=initial;
   int i, j;
@@ -17,7 +18,38 @@ unsigned int calc_crc_stream(const unsigned char *data, const int datalen, const
 }
 
 // CCITT CRC16 (Floppy Disk Data)
-unsigned int calc_crc(const unsigned char *data, const int datalen)
+uint16_t calc_crc(const unsigned char *data, const int datalen)
 {
   return (calc_crc_stream(data, datalen, 0xffff, 0x1021));
+}
+
+// Configurable CRC32 stream algorithm
+uint32_t calc_crc32_stream(const unsigned char *data, const int datalen, const uint32_t initial, const uint32_t polynomial)
+{
+  uint32_t crc=initial;
+  int i, j;
+
+  for (i=0; i<datalen; i++)
+  {
+    char ch=data[i];
+
+    for (j=0; j<8; j++)
+    {
+      uint32_t b=(ch^crc)&1;
+
+      crc>>=1;
+
+      if(b) crc=crc^polynomial;
+
+      ch>>=1;
+    }
+  }
+
+  return ~crc;
+}
+
+// CRC-32b (ISO 3309)
+uint32_t calc_crc32(const unsigned char *data, const int datalen)
+{
+  return (calc_crc32_stream(data, datalen, 0xffffffff, 0xedb88320));
 }
--- tools/crc.h
+++ tools/crc.h
@@ -1,7 +1,12 @@
 #ifndef _CRC_H_
 #define _CRC_H_
 
-extern unsigned int calc_crc_stream(const unsigned char *data, const int datalen, const unsigned int initial, const unsigned int polynomial);
-extern unsigned int calc_crc(const unsigned char *data, const int datalen);
+#include <stdint.h>
+
+extern uint16_t calc_crc_stream(const unsigned char *data, const int datalen, const uint16_t initial, const uint16_t polynomial);
+extern uint16_t calc_crc(const unsigned char *data, const int datalen);
+
+extern uint32_t calc_crc32_stream(const unsigned char *data, const int datalen, const uint32_t initial, const uint32_t polynomial);
+extern uint32_t calc_crc32(const unsigned char *data, const int datalen);
 
 #endif
