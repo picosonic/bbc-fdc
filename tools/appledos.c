45dbacea95cea1ce4ea5b60e5a57131fd06794f1
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2020-12-01 16:19:05 +0000

    Add allocation bitmap

--- tools/appledos.c
+++ tools/appledos.c
@@ -39,6 +39,7 @@ int appledos_validate()
   {
     unsigned char sniff[APPLEGCR_SECTORLEN];
     struct appledos_vtoc *vtoc;
+    int track;
 
     // Copy VTOC to sniff buffer
     memcpy(sniff, sector0->data, sector0->datasize);
@@ -56,7 +57,23 @@ int appledos_validate()
     printf("Sectors/Track: %d\n", vtoc->sectorspertrack); // 13 or 16
     printf("Bytes/Sector: %d\n", (vtoc->bytespersector[1]<<8)|vtoc->bytespersector[0]); // LO/HI
 
-    // TODO loop through the allocation bitmaps (one per track)
+    // Loop through the allocation bitmaps (one per track)
+    if (vtoc->tracksperdisk<=35)
+    {
+      struct appledos_alloc_bitmap *alloc;
+
+      for (track=0; track<vtoc->tracksperdisk; track++)
+      {
+        // Map VTOC to sniff buffer
+        alloc=(struct appledos_vtoc *)&sniff[0x38+(track*4)];
+
+        // Bitmap bytes 2 and 3 are not used
+        // If a bit is set in bitmap, then sector is not allocated
+        // Bit order is byte 0=FEDCBA98, byte 1=76543210
+        if ((alloc->bitmap[2]==0) && (alloc->bitmap[3]==0))
+          printf("T%d %.2x %.2x\n", track, alloc->bitmap[0], alloc->bitmap[1]);
+      }
+    }
   }
 
   return format;
