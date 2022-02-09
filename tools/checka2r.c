c5fdb55f4434895e6ea001aa1a3423de9bf17a91
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2022-02-09 10:34:24 +0000

    Added processing of META chunk

--- tools/checka2r.c
+++ tools/checka2r.c
@@ -23,6 +23,104 @@ int a2r_processheader(FILE *fp)
   return 0;
 }
 
+int a2r_processmeta(struct a2r_chunkheader *chunkheader, FILE *fp)
+{
+  uint8_t *meta;
+
+  meta=malloc(chunkheader->size);
+
+  if (meta==NULL)
+  {
+    printf("Failed to allocate memory for META block\n");
+
+    return 1;
+  }
+
+  if (fread(meta, 1, chunkheader->size, fp)==chunkheader->size)
+  {
+    uint16_t i;
+
+    printf("  ");
+    for (i=0; i<chunkheader->size; i++)
+    {
+      printf("%c", meta[i]);
+
+      if (meta[i]=='\n')
+        printf("  ");
+    }
+    printf("\n");
+
+    free(meta);
+
+    return 0;
+  }
+
+  free(meta);
+
+  return 1;
+}
+
+int a2r_processinfo(struct a2r_chunkheader *chunkheader, FILE *fp)
+{
+  struct a2r_info *info;
+  uint16_t i;
+
+  info=malloc(chunkheader->size);
+
+  if (info==NULL)
+  {
+    printf("Failed to allocate memory for INFO block\n");
+
+    return 1;
+  }
+
+  fread(info, 1, chunkheader->size, fp);
+
+  printf("  Version: %d\n", info->version);
+
+  printf("  Creator: ");
+  for (i=0; i<sizeof(info->creator); i++)
+    printf("%c", info->creator[i]);
+  printf("\n");
+
+  printf("  Disk type: %d (%s)\n", info->disktype, info->disktype==1?"5.25\"":"3.5\"");
+
+  printf("  Protection: Write%s\n", info->writeprotected==1?"able":" protected");
+
+  printf("  Sync: %s\n", info->synchronised==1?"Cross track sync":"None");
+
+  free(info);
+
+  return 0;
+}
+
+int a2r_processchunk(struct a2r_chunkheader *chunkheader, FILE *fp)
+{
+  uint16_t i;
+
+  printf("Chunk '");
+  for (i=0; i<4; i++)
+    printf("%c", chunkheader->id[i]);
+    
+  printf("', length %d\n", chunkheader->size);
+
+  if (strncmp((char *)&chunkheader->id, A2R_CHUNK_INFO, 4)==0)
+  {
+    if (a2r_processinfo(chunkheader, fp)!=0)
+      return 1;
+  }
+  else
+  if (strncmp((char *)&chunkheader->id, A2R_CHUNK_META, 4)==0)
+  {
+    if (a2r_processmeta(chunkheader, fp)!=0)
+      return 1;
+  }
+  else
+    fseek(fp, chunkheader->size, SEEK_CUR);
+
+  return 0;
+}
+
 int main(int argc, char **argv)
 {
   FILE *fp;
@@ -52,49 +150,17 @@ int main(int argc, char **argv)
   while (!feof(fp))
   {
     struct a2r_chunkheader chunkheader;
-    uint16_t i;
 
     if (fread(&chunkheader, 1, sizeof(chunkheader), fp)<=0)
       break;
 
-    printf("Chunk '");
-    for (i=0; i<4; i++)
-      printf("%c", chunkheader.id[i]);
-    
-    printf("', length %d\n", chunkheader.size);
-
-    if (strncmp((char *)&chunkheader.id, A2R_CHUNK_INFO, 4)==0)
+    if (a2r_processchunk(&chunkheader, fp)!=0)
     {
-      struct a2r_info *info;
+      // Something went wrong
+      fclose(fp);
 
-      info=malloc(chunkheader.size);
-
-      if (info==NULL)
-      {
-        printf("Failed to allocate memory for INFO block\n");
-        fclose(fp);
-        return 1;
-      }
-
-      fread(info, 1, chunkheader.size, fp);
-
-      printf("  Version: %d\n", info->version);
-
-      printf("  Creator: ");
-      for (i=0; i<sizeof(info->creator); i++)
-        printf("%c", info->creator[i]);
-      printf("\n");
-
-      printf("  Disk type: %d (%s)\n", info->disktype, info->disktype==1?"5.25\"":"3.5\"");
-
-      printf("  Protection: Write%s\n", info->writeprotected==1?"able":" protected");
-
-      printf("  Sync: %s\n", info->synchronised==1?"Cross track sync":"None");
-
-      free(info);
+      return 1;
     }
-    else
-      fseek(fp, chunkheader.size, SEEK_CUR);
   }
 
   fclose(fp);
