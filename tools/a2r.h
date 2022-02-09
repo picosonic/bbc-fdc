90185f218e2e45bf9b8d513346d61fdfe9088c60
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2022-02-09 00:18:15 +0000

    Added first draft of a2r (Applesauce) parser

---
+++ tools/a2r.h
@@ -0,0 +1,75 @@
+#ifndef _A2R_H_
+#define _A2R_H_
+
+// A2R v2.0 (Aug 12 2018)
+// All data is stored little-endian
+//
+// STRM
+//
+// For timing/xtiming, data is stored as a stream of unsigned bytes (aligned to index), each byte representing a single flux transition, and is the number of "ticks" since the last flux transition. Ticks are 125 nanoseconds, so 4uS is 32 ticks. 0xff means keep adding a non-0xff is seen. Timing is 250ms, xtiming is 450ms.
+//
+// For bits, data is a 16384 byte bitstream. This is a series of bits recorded from drive and normalised to 4uS intervals. High bit first, low bit last. Estimated loop point is number of ticks from start of capture (used to determine track length). Deprecated in Applesauce v1.0.3.
+//
+// META
+//
+// Tab-delimited UTF8 list of key value pairs. All rows end with a linefeed (0x0a), multiple values are pipe-separated. Empty values allowed for standard keys. Keys are case sensitive. No duplicate keys.
+//
+// Standard metadata keys
+// Key               Purpose
+// title             Name/Title
+// subtitle          Subtitle
+// publisher         Publisher
+// developer         Developer
+// copyright         Copyright date
+// version           Version number
+// language          Language
+// requires_ram      RAM requirements
+// requires_machine  Which computer does this run on
+// notes             Additional notes
+// side              Physical disk side (as "Disk #, Side [A|B]")
+// side_name         Name of the disk side
+// contributor       Name of the person who imaged the disk
+// image_date        ISO8601 date of imaging (YYYY-MM-DDTHH:MM:SS.HHHZ)
+
+#define A2R_MAGIC "A2R2"
+
+// Chunk ids
+#define A2R_CHUNK_INFO "INFO"
+#define A2R_CHUNK_STRM "STRM"
+#define A2R_CHUNK_META "META"
+
+#pragma pack(push,1)
+
+struct a2r_header
+{
+  uint8_t id[4]; // "A2R2"
+  uint8_t ff; // 0xff to prevent 7-bit transmission
+  uint8_t lfcrlf[3]; // LF CR LF (0x0a 0x0d 0x0a)
+};
+
+struct a2r_chunkheader
+{
+  uint8_t id[4]; // 4 ASCII char for chunk id
+  uint32_t size; // Size of chunk in bytes
+};
+
+struct a2r_info
+{
+  uint8_t version; // INFO chunk revision (currently 1)
+  uint8_t creator[32]; // Name of software which created the file, UTF8, no BOM, padded with spaces
+  uint8_t disktype; // 1=5.25", 2=3.5"
+  uint8_t writeprotected; // 1=Floppy is write protected
+  uint8_t synchronised; // 1=Cross track sync was used during imaging
+};
+
+struct a2r_strm
+{
+  uint8_t location; // Track where capture happened (5.25" in quarter tracks, 3.5" in tracks where 0-79 is side 0 and 80-159 is for side 1), 0xff indicates end of STRM
+  uint8_t type; // Capture type, 1=Timing, 2=Bits, 3=xtiming
+  uint32_t size; // Size of data in bytes
+  uint32_t loop; // Duration until sync sensor was triggered at loop point
+};
+
+#pragma pack(pop)
+
+#endif
---
+++ tools/checka2r.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "a2r.h"
+
+struct a2r_header a2rheader;
+
+int a2r_processheader(FILE *fp)
+{
+  fread(&a2rheader, 1, sizeof(a2rheader), fp);
+
+  if (strncmp((char *)&a2rheader.id, A2R_MAGIC, strlen(A2R_MAGIC))!=0)
+    return 1;
+
+  if (a2rheader.ff!=0xff)
+    return 1;
+
+  if ((a2rheader.lfcrlf[0]!=0x0a) || (a2rheader.lfcrlf[1]!=0x0d) || (a2rheader.lfcrlf[2]!=0x0a))
+    return 1;
+
+  return 0;
+}
+
+int main(int argc, char **argv)
+{
+  FILE *fp;
+
+  if (argc!=2)
+  {
+    printf("Specify .a2r on command line\n");
+    return 1;
+  }
+
+  fp=fopen(argv[1], "rb");
+  if (fp==NULL)
+  {
+    printf("Unable to open file\n");
+    return 2;
+  }
+
+  if (a2r_processheader(fp)!=0)
+  {
+    fclose(fp);
+    return 1;
+  }
+
+  printf("A2R file\n");
+
+  // Process chunks
+  while (!feof(fp))
+  {
+    struct a2r_chunkheader chunkheader;
+    int i;
+
+    if (fread(&chunkheader, 1, sizeof(chunkheader), fp)<=0)
+      break;
+
+    printf("Chunk '");
+    for (i=0; i<4; i++)
+      printf("%c", chunkheader.id[i]);
+    
+    printf("', length %d\n", chunkheader.size);
+
+    if (strncmp((char *)&chunkheader.id, A2R_CHUNK_INFO, 4)==0)
+    {
+      struct a2r_info info;
+
+      fread(&info, 1, sizeof(info), fp);
+
+      printf("  Version: %d\n", info.version);
+
+      printf("  Creator: ");
+      for (i=0; i<sizeof(info.creator); i++)
+        printf("%c", info.creator[i]);
+      printf("\n");
+
+      printf("  Disk type: %d (%s)\n", info.disktype, info.disktype==1?"5.25\"":"3.5\"");
+
+      printf("  Protection: Write%s\n", info.writeprotected==1?"able":" protected");
+
+      printf("  Sync: %s\n", info.synchronised==1?"Cross track sync":"None");
+    }
+    else
+      fseek(fp, chunkheader.size, SEEK_CUR);
+  }
+
+  fclose(fp);
+
+  return 0;
+}
